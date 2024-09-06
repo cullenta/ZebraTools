c6d77991922c9e62fccbf624c728f5f27cc1e8f2
cullenta <taite.cullen@mail.utoronto.ca>
2024-09-06 15:12:24 -0400

    added launch and install

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chmod +x launch.sh
+# Change directory
+cd 'Slip Maker Add-in'
+npm install -g yo generator-office
+yo office
---
+++ launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+chmod +x launch.sh
+# Change directory
+cd 'Slip Maker Add-in'
+npm start
